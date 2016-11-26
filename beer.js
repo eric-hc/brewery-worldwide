bf4e3382ba2b9a37d4c97e4ca2cb5268d53e5947
Eric Carboni <ericcarboni@gmail.com>
2016-11-26 16:53:47 -0500

    Display results on webpage

--- beer.js
+++ beer.js
@@ -1,13 +1,19 @@
 function searchBrewery() {
-    var query = document.getElementById("city_query").value;
+    var query = document.getElementById("brewery_query").value;
     $.ajax({
     type: "GET",
     url: "http://localhost:3000/test?query=" + query,
     success: function(result) {
         console.log(result);
+        var output  = "";
+        for (item in result) {
+            output += result[item].name + "\n";
+        }
+        document.getElementById("output").textContent=(output);
     },
     error: function(result) {
         console.log("Error");
+        document.getElementById("output").textContent=("Something went wrong");
     }
     });
 }
